a07e9028d3889b55a93417ec469def7eb59ab8d1
frankujcv <francklin.marroquin@ujcv.edu.hn>
2021-11-30 14:31:32 -0600

    creating model file

---
+++ models/Compras.php
@@ -0,0 +1,17 @@
+<?php
+class Compra{
+    private $connection;
+    private $table = 'compras';
+
+    public $idCompra;
+    public $idUsuario;
+    public $idProveedor;
+    public $fechaHora;
+
+    public function __construct($db)
+    {
+        
+    }
+}
+?>
+
