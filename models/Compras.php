084db85a7a50f846b182a4226fb924d72e38a819
frankujcv <francklin.marroquin@ujcv.edu.hn>
2021-11-30 23:31:03 -0600

    crear compras

--- models/Compras.php
+++ models/Compras.php
@@ -1,7 +1,7 @@
 <?php
 class Compra{
     private $connection;
-    private $table = 'compras';
+    private $tabla = 'compras';
 
     public $idCompra;
     public $idUsuario;
@@ -10,7 +10,30 @@ class Compra{
 
     public function __construct($db)
     {
-        
+       $this->connection = $db; 
+    }
+
+    public function create()
+    {
+        $query = 'INSERT INTO ' .  $this->tabla . ' 
+        SET idUsuario = :idUsuario,
+        idProveedor = :idProveedor,
+        fechaHora = :fechaHora';
+        $this->fechaHora = $this->fechaHora->format('Y-m-d H:m:s');
+        $stmt = $this->connection->prepare($query); 
+        $this->idUsuario = htmlspecialchars(strip_tags($this->idUsuario));
+        $this->idProveedor = htmlspecialchars(strip_tags($this->idProveedor));
+
+        $stmt->bindParam(':idUsuario', $this->idUsuario );
+        $stmt->bindParam(':idProveedor', $this->idProveedor);
+        $stmt->bindParam(':fechaHora', $this->fechaHora);
+
+        try{
+            $stmt->execute();
+            return true;
+        }catch(PDOException $err){
+            return $err;
+        }
     }
 }
 ?>
